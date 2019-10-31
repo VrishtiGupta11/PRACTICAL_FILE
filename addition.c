c24e3be4f1fd63822ff1d2a21dc513b498b3622c
VrishtiGupta11 <53931644+VrishtiGupta11@users.noreply.github.com>
2019-10-31 19:50:56 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter any two integral numbers : ");
+scanf("%d %d",&a,&b);
+c = a + b;
+printf("the sum of two numbers is %d\n",c);
+return 0;
+}
